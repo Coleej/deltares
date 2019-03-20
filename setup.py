92cd3c63f9cb84ad5f00aaff413846b6ed98512c
Cody Johnson <codyjohnson144@gmail.com>
2019-03-20 17:22:42 -0500

    finished package setup

--- setup.py
+++ setup.py
@@ -11,7 +11,21 @@ setup(name='deltares',
       description='modules for working with Delft3D and XBeach output',
       long_description=long_description,
       long_description_content_type='text/markdown',
+      classifiers=[
+          'Development Status :: 1 - Planning',
+          'Environment :: Plugins',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Scientific/Engineering'
+      ],
       url='',
       license='MIT',
       packages=find_packages(),
+      install_requires=[
+          'numpy',
+          'scipy'
+      ],
+      include_package_data=True,
       zip_safe=False)
